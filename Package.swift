2070a62c73fa0c58d9b74096131931dcfa7f6149
Leerie Simpson <leerie.a.simpson@gmail.com>
2020-02-11 01:18:39 -0500

    Prepd for stardust

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "GrammarKit",
+    platforms: [
+        .macOS("10.15"), .iOS("13.0"),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
